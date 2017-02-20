f31f50edb8a0dcf82a96cd98dd9ef18fa0dfc271
chenming <583431043@qq.com>
2017-02-20 11:52:42 +0800

    YouTubeLayout

---
+++ app/src/main/java/com/qicode/draghelperdr/YouTubeLayout.java
@@ -0,0 +1,163 @@
+package com.qicode.draghelperdr;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by chenming on 17/2/20.
+ */
+
+public class YouTubeLayout extends ViewGroup{
+
+    private ViewDragHelper mDragHelper;
+    private View mHeaderView;
+    private View mDescView;
+
+    private float mInitialMotionX;
+    private float mInitialMotionY;
+
+    private int mDragRange;
+    private int mTop;
+    private float mDragOffset;
+
+    public YouTubeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
+    }
+
+    public YouTubeLayout(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mHeaderView = findViewById(R.id.viewHeader);
+        mDescView = findViewById(R.id.viewDesc);
+        mDescView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "mDescView", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mHeaderView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "mHeaderView", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private class DragHelperCallback extends ViewDragHelper.Callback{
+
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return child == mHeaderView;
+        }
+
+        //捕获的view位置移动时，View的变化处理
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            mTop = top;
+            mDragOffset = (float)top/mDragRange;//滑动percent
+            mHeaderView.setPivotX(mHeaderView.getWidth());//右下角锚点缩放
+            mHeaderView.setPivotY(mHeaderView.getHeight());
+            mHeaderView.setScaleX(1 - mDragOffset / 2);
+            mHeaderView.setScaleY(1 - mDragOffset / 2);
+            mDescView.setAlpha(1 - mDragOffset);
+            requestLayout();
+            Log.e("TAG", "onViewPositionChanged");
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            int top = getPaddingTop();
+            if (yvel > 0 || (yvel == 0 && mDragOffset > 0.5f)) {//向下沉底条件
+                top += mDragRange;
+            }
+            mDragHelper.settleCapturedViewAt(releasedChild.getLeft(), top);
+            invalidate();//开启惯性
+            Log.e("TAG", "onViewReleased");
+        }
+
+        @Override
+        public int getViewVerticalDragRange(View child) {//使子View的点击事件生效
+            return mDragRange;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            final int topBound = getPaddingTop();
+            final int bottomBound = getHeight() - mHeaderView.getHeight() - mHeaderView.getPaddingBottom();
+
+            final int newTop = Math.min(Math.max(top, topBound), bottomBound);
+            return newTop;
+        }
+
+    }
+
+    //惯性滚动
+    @Override
+    public void computeScroll() {
+        if(mDragHelper.continueSettling(true)){
+            invalidate();
+        }
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int maxHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, 0),
+                resolveSizeAndState(maxHeight, heightMeasureSpec, 0));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mDragRange = getHeight() - mHeaderView.getHeight();
+
+        mHeaderView.layout(
+                0,
+                mTop,
+                r,
+                mTop + mHeaderView.getMeasuredHeight());
+
+        mDescView.layout(
+                0,
+                mTop + mHeaderView.getMeasuredHeight(),
+                r,
+                mTop  + b);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mDragHelper.cancel();
+            return false;
+        }
+        return mDragHelper.shouldInterceptTouchEvent(ev);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        mDragHelper.processTouchEvent(ev);
+        return true;
+    }
+
+
+}
